7930bc3ac2b50888a52ee4709f22b4abd432f599
toyama1710 <toyama@toyama1710.net>
2022-04-09 07:47:52 +0900

    TODO: redering colophon

--- src/pdf_util.rs
+++ src/pdf_util.rs
@@ -1,16 +1,57 @@
+use crate::book::ToC;
 use anyhow::{Context, Result};
 use printpdf::image_crate::io::Reader as ImageReader;
 use printpdf::image_crate::DynamicImage;
 use printpdf::scale::Mm;
 use printpdf::Image;
 use printpdf::ImageTransform;
+use printpdf::IndirectFontRef;
 use printpdf::PdfLayerReference;
 use std::fs::File;
 use std::io::BufReader;
 
+pub fn rendering_table_of_contents(
+    layer: &PdfLayerReference,
+    font: &IndirectFontRef,
+    toc: &ToC,
+    print_width: Mm,
+    print_height: Mm,
+) {
+    layer.set_font(font, 24.0);
+    layer.set_line_height(30.0);
+    layer.set_text_cursor(print_width / 5.0, print_height / 8.0 * 7.0);
+
+    layer.write_text(&toc.title, &font);
+    layer.add_line_break();
+    layer.write_text("目次", &font);
+    layer.add_line_break();
+
+    layer.set_font(font, 20.0);
+    layer.write_text(format!("  表紙:   {}", toc.front_author), &font);
+    layer.add_line_break();
+    layer.write_text(format!("  裏表紙: {}", toc.back_author), &font);
+    layer.add_line_break();
+
+    for (range, title, author) in &toc.body {
+        if range.len() <= 1 {
+            layer.write_text(format!("  P{}: ", range.start), &font);
+        } else {
+            layer.write_text(format!("  P{}-{}: ", range.start, range.end - 1), &font);
+        }
+
+        if let Some(title) = title {
+            layer.write_text(format!("「{}」", title), &font);
+        }
+
+        layer.write_text(format!("{}", author), &font);
+
+        layer.add_line_break();
+    }
+}
+
 pub fn add_image(
-    img: &File,
     layer: PdfLayerReference,
+    img: &File,
     pos_x: Mm,
     pos_y: Mm,
     print_width: Mm,
--- src/write_pdf.rs
+++ src/write_pdf.rs
@@ -6,6 +6,7 @@ use anyhow::Result;
 use printpdf::scale::Mm;
 use printpdf::PdfDocument;
 use printpdf::PdfDocumentReference;
+use std::io::Cursor;
 
 pub fn construct_browse_pdf(book: &Book, sz: Size) -> Result<PdfDocumentReference> {
     let mut layer_idx = 0;
@@ -13,16 +14,18 @@ pub fn construct_browse_pdf(book: &Book, sz: Size) -> Result<PdfDocumentReferenc
     let (doc, p, l) =
         PdfDocument::new("for browsing", width, height, format!("layer{}", layer_idx));
 
+    let mut font_reader = Cursor::new(include_bytes!("../assets/MyricaM.TTC").as_ref());
+    let font = doc.add_external_font(&mut font_reader).unwrap();
+
     let layer = doc.get_page(p).get_layer(l);
     add_image(
-        &book.front_cover,
         layer.clone(),
+        &book.front_cover,
         Mm(0.0),
         Mm(0.0),
         width,
         height,
     )?;
-
     for page in &book.page[1..] {
         layer_idx += 1;
         let (p, l) = doc.add_page(width, height, format!("layer{}", layer_idx));
@@ -30,18 +33,20 @@ pub fn construct_browse_pdf(book: &Book, sz: Size) -> Result<PdfDocumentReferenc
         match page {
             BackCover => {
                 add_image(
-                    &book.back_cover,
                     layer.clone(),
+                    &book.back_cover,
                     Mm(0.0),
                     Mm(0.0),
                     width,
                     height,
                 )?;
             }
-            ToC => {}
-            Colophon => {}
+            ToC => {
+                rendering_table_of_contents(&layer, &font, &book.toc, width, height);
+            }
+            Colophon => { /*rendering_colophon();*/ }
             BodyImg(f) => {
-                add_image(f, layer.clone(), Mm(0.0), Mm(0.0), width, height)?;
+                add_image(layer.clone(), f, Mm(0.0), Mm(0.0), width, height)?;
             }
             Blank => {}
             _ => panic!("unreachable pattern!"),
