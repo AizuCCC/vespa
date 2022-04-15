852ab59fe87d6a5b17a64febed41320f40186e4b
toyama1710 <toyama@toyama1710.net>
2022-04-15 22:33:20 +0900

    pdf生成部分を並列化

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use clap::{command, Arg};
 use std::fs::File;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::thread;
 use vespa::book::*;
 use vespa::construct_pdf::*;
 use vespa::main_util::*;
@@ -22,26 +24,51 @@ fn main() -> Result<()> {
     canonicalize_path(&mut config, book_dir.as_path())?;
     validate_path(&config)?;
 
-    let book = pagenation(&config)?;
+    let config = Arc::new(config);
+    let book = Arc::new(pagenation(&config)?);
+    let mut v = vec![];
 
-    let mut pdf_path = book_dir;
-    pdf_path.push("book.pdf");
-    let doc = construct_view_pdf(&book, config.size.clone())?;
-    doc.save(&mut std::io::BufWriter::new(File::create(&pdf_path)?))?;
-    pdf_path.pop();
+    {
+        let mut pdf_path = book_dir.clone();
+        let config = Arc::clone(&config);
+        let book = Arc::clone(&book);
+        v.push(thread::spawn(move || -> Result<()> {
+            pdf_path.push("book.pdf");
+            let doc = construct_view_pdf(&book, config.size.clone())?;
+            doc.save(&mut std::io::BufWriter::new(File::create(pdf_path.as_path())?))?;
+            Ok(())
+        }));
+    }
 
-    pdf_path.push("cover.pdf");
-    let doc = construct_cover(&book, config.size.clone())?;
-    doc.save(&mut std::io::BufWriter::new(File::create(&pdf_path)?))?;
-    pdf_path.pop();
+    {
+        let mut pdf_path = book_dir.clone();
+        let config = Arc::clone(&config);
+        let book = Arc::clone(&book);
+        v.push(thread::spawn(move || -> Result<()> {
+            pdf_path.push("cover.pdf");
+            let doc = construct_cover(&book, config.size.clone())?;
+            doc.save(&mut std::io::BufWriter::new(File::create(pdf_path.as_path())?))?;
+            Ok(())
+        }));
+    }
 
-    pdf_path.push("body.pdf");
-    let doc = construct_body(&book, config.size.clone())?;
-    doc.save(&mut std::io::BufWriter::new(File::create(&pdf_path)?))?;
-    pdf_path.pop();
+    {
+        let mut pdf_path = book_dir.clone();
+        let config = Arc::clone(&config);
+        let book = Arc::clone(&book);
+        v.push(thread::spawn(move || -> Result<()> {
+            pdf_path.push("body.pdf");
+            let doc = construct_body(&book, config.size.clone())?;
+            doc.save(&mut std::io::BufWriter::new(File::create(pdf_path.as_path())?))?;
+            Ok(())
+        }));
+    }
 
-    // concat(cover, body)
-    // save("print.pdf")
+    for h in v {
+        if let Err(e) = h.join().unwrap() {
+            println!("{:?}", e);
+        }
+    }
 
     Ok(())
 }
